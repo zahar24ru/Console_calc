f5e88114e25b4a9207cae07836894c05b57684d2
Kostia <zaharovkostia@yandex.ru>
2021-07-02 19:46:19 +0700

    Calc v1.1

--- src/ru/geekbrains/lesson2/Main.java
+++ src/ru/geekbrains/lesson2/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Выбор операций \n 1. Сложение \n 2. Вычитание \n 3. Умножение \n 4. Деление \n 5. Остаток");
         Scanner scanner = new Scanner(System.in);
-        int a,b,operation, result;
+        int a,b,operation,result;
         String text = "WTF";
         operation = scanner.nextInt();
         System.out.println("Введите число А ");
@@ -27,7 +27,7 @@ public class Main {
             result = a % b;
         }else {
 
-            System.out.println("Допущена ошибка в выборе действия" + operation);}
+            System.out.println("Допущена ошибка в выборе действия. \nДействие № "  + operation + " Не найдено");}
         System.out.println("Результат = " + result);
 
     }
